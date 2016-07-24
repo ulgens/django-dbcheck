25b9e4c4529e05b59154f269aeaf6bebb161b122
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2016-07-25 00:29:45 +0300

    Init.

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+========
+Usage
+========
+
+To use django dbcheck in a project::
+
+    import django_dbcheck
---
+++ tests/test_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_django-dbcheck
+------------
+
+Tests for `django-dbcheck` models module.
+"""
+
+from django.test import TestCase
+
+from django_dbcheck import models
+
+
+class TestDjango_dbcheck(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
